2dde3ab2c988363bf7a7a0228614ad9030b139a7
Deependra-009 <deependra04trivedi@gmail.com>
2025-08-02 00:54:50 +0530

    add docker file  + kubernets configuration

--- FSE_APP_DEPLOYMENT/Project-2(Shopping)/api-gateway-server/src/main/java/com/shopping/api_gateway_server/controller/ApiGatewayController.java
+++ FSE_APP_DEPLOYMENT/Project-2(Shopping)/api-gateway-server/src/main/java/com/shopping/api_gateway_server/controller/ApiGatewayController.java
@@ -1,10 +1,19 @@
 package com.shopping.api_gateway_server.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiGatewayController {
+
+    @Value("${my.custom.value:default}")
+    private String customValue;
+
+    @GetMapping("/test-api")
+    public String testAPI(){
+        return "API-GATEWAY FINE "+customValue;
+    }
 
     @GetMapping("/orderServiceFallBack")
     public String orderServiceFallBack(){
