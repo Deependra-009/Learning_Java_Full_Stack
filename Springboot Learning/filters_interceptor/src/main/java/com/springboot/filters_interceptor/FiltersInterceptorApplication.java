893d8fc17e67e13cf68dfcec0a2caebe2f8aebfd
Deependra-009 <deependra04trivedi@gmail.com>
2025-08-20 19:56:56 +0530

    Interceptors & Filters

---
+++ Springboot Learning/filters_interceptor/src/main/java/com/springboot/filters_interceptor/FiltersInterceptorApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.filters_interceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiltersInterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiltersInterceptorApplication.class, args);
+	}
+
+}
